f89764ce6a66d9b15ab1208aa7091a111246f5ec
Alexeykaa <ka.alexey@gmail.com>
2023-10-16 17:46:22 +0300

    init implementation

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task3;
 
+import java.time.DateTimeException;
+import java.time.YearMonth;
+
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        try {
+            YearMonth date = YearMonth.now().withMonth(month);
+            System.out.println(date.lengthOfMonth());
+        } catch (DateTimeException de) {
+            System.out.println("wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        symbol = Character.toLowerCase(symbol);
+        if (symbol < 'a' || symbol > 'z') {
+            System.out.println("Non English");
+        } else {
+            System.out.println("English");
+        }
     }
 }
